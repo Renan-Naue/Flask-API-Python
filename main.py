b526932752eee7027da37c6b19671880bb7f939a
Renan <renan_naue2@hotmail.com>
2022-01-05 16:29:12 -0300

    Add post and simple user validation

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, Response
 
 empregados =[ 
             {'nome' : 'Valentina', 'cargo' : 'analista', 'salario' : 5000},
-            {'nome' : 'Ze', 'cargo' : 'policial', 'salario' : 4500}, 
+            {'nome' : 'Ze', 'cargo' : 'policial', 'salario' : 5000}, 
             {'nome' : 'Joao', 'cargo' : 'policial', 'salario' : 12000 },
 ]
+
+users = [
+            {'username' : 'Renan', 'secret' : '123456'}
+
+        ]
+
+
+def check_user(username, secret):
+    for user in users:
+        if (user['username'] == username) and (user['secret'] == secret):
+            return True
+
+    return False
 
 app = Flask(__name__)
 
@@ -12,9 +25,11 @@ app = Flask(__name__)
 def home():
     return '<h1>Home page</h1>'
 
+
 @app.route('/empregados')
 def get_empregados():
     return {'empregados' : empregados}
+
 
 @app.route('/empregados/<cargo>')
 def get_empregados_cargo(cargo):
@@ -23,6 +38,7 @@ def get_empregados_cargo(cargo):
         if cargo == empregado['cargo'].lower():
             out_empregados.append(empregado)
     return {'empregados' : out_empregados}
+
 
 @app.route('/empregados/<info>/<value>')
 def get_empregados_info(info, value):
@@ -39,6 +55,34 @@ def get_empregados_info(info, value):
                     out_empregados.append(empregado)
     
     return {'empregados' : out_empregados}
+
+
+@app.route('/informations', methods=['POST'])
+def get_empregados_post():
+    username = request.form['username']
+    secret = request.form['secret']
+
+    
+    if not check_user(username, secret):
+        return Response('Unauthorized', status=401)
+
+    info = request.form['info']
+    value = request.form['value']
+    
+    out_empregados = []
+    for empregado in empregados:
+        if info in empregado.keys():
+            value_empregado = empregado[info]
+
+            if type(value_empregado) == str:
+                if value == value_empregado.lower():
+                    out_empregados.append(empregado)
+            if type(value_empregado) == int:
+                if int(value) == value_empregado:
+                    out_empregados.append(empregado)
+    
+    return {'empregados' : out_empregados}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
