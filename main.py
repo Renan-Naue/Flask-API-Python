718e9538889ce3fcda808fc4000e05d9f1eb207f
Renan <renan_naue2@hotmail.com>
2022-01-05 15:19:32 -0300

    Second commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+empregados =[ 
+            {'nome' : 'Valentina', 'cargo' : 'analista', 'salario' : 5000},
+            {'nome' : 'Ze', 'cargo' : 'policial', 'salario' : 4500}, 
+            {'nome' : 'Joao', 'cargo' : 'policial', 'salario' : 12000 },
+]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Home page</h1>'
+
+@app.route('/empregados')
+def get_empregados():
+    return {'empregados' : empregados}
+
+@app.route('/empregados/<cargo>')
+def get_empregados_cargo(cargo):
+    out_empregados = []
+    for empregado in empregados:
+        if cargo == empregado['cargo'].lower():
+            out_empregados.append(empregado)
+    return {'empregados' : out_empregados}
+
+@app.route('/empregados/<info>/<value>')
+def get_empregados_info(info, value):
+    out_empregados = []
+    for empregado in empregados:
+        if info in empregado.keys():
+            value_empregado = empregado[info]
+
+            if type(value_empregado) == str:
+                if value == value_empregado.lower():
+                    out_empregados.append(empregado)
+            if type(value_empregado) == int:
+                if int(value) == value_empregado:
+                    out_empregados.append(empregado)
+    
+    return {'empregados' : out_empregados}
+
+if __name__ == '__main__':
+    app.run(debug=True)
